c50463f0c57972d01f8738f9b0cf5b5a51309a2d
Mradul Singh <msingh@dealmeridian.com>
2026-01-22 02:26:14 +0530

    Auto-sync [02:26]: nvim

--- nvim/lua/plugins/jupyter.lua
+++ nvim/lua/plugins/jupyter.lua
@@ -1,59 +1,46 @@
 -- Jupyter Notebook Support
--- Two modes:
--- 1. jupytext.nvim: Open .ipynb files as readable Python/Markdown (native editing)
--- 2. Jupynium: Full browser-synced notebook experience with execution
+-- nvim-jupyter-client: Fast native .ipynb editing (no external conversion)
+-- Jupynium: Browser-synced execution (optional)
 
 return {
-  -- jupytext.nvim: Native .ipynb viewing/editing
-  -- Opens notebooks as readable text format, auto-saves back to .ipynb
+  -- Native .ipynb editing - fast, no external tools needed
   {
-    "goerz/jupytext.nvim",
-    version = "0.2",
-    opts = {
-      format = "py:percent", -- Convert to Python with # %% cell markers
-      update = true, -- Preserve outputs when saving
-      autosync = true, -- Auto-sync paired files
+    "geg2102/nvim-jupyter-client",
+    ft = { "ipynb" },
+    config = function()
+      require("nvim-jupyter-client").setup({})
+    end,
+    keys = {
+      { "<leader>ja", "<cmd>JupyterAddCellBelow<cr>", desc = "Add cell below", ft = "ipynb" },
+      { "<leader>jA", "<cmd>JupyterAddCellAbove<cr>", desc = "Add cell above", ft = "ipynb" },
+      { "<leader>jd", "<cmd>JupyterRemoveCell<cr>", desc = "Delete cell", ft = "ipynb" },
+      { "<leader>jt", "<cmd>JupyterConvertCellType<cr>", desc = "Toggle cell type", ft = "ipynb" },
+      { "<leader>jm", "<cmd>JupyterMergeCellBelow<cr>", desc = "Merge cell below", ft = "ipynb" },
+      { "<leader>jM", "<cmd>JupyterMergeCellAbove<cr>", desc = "Merge cell above", ft = "ipynb" },
     },
   },
 
-  -- Jupynium: Browser-synced notebook (for execution)
+  -- Jupynium: For executing cells via browser (optional)
   {
     "kiyoon/jupynium.nvim",
     build = "uv tool install . --force",
-    dependencies = {
-      "stevearc/dressing.nvim",
-    },
+    cmd = { "JupyniumStartAndAttachToServer", "JupyniumStartSync" },
     config = function()
       require("jupynium").setup({
         python_host = vim.g.python3_host_prog or "python3",
         default_notebook_URL = "localhost:8888/nbclassic",
-        jupyter_command = "jupyter",
-
-        auto_start_server = { enable = false, file_pattern = { "*.ju.py" } },
-        auto_attach_to_server = { enable = true, file_pattern = { "*.ju.py" } },
-        auto_start_sync = { enable = false, file_pattern = { "*.ju.py" } },
         auto_download_ipynb = true,
-        auto_close_tab = true,
-
-        autoscroll = { enable = true, mode = "always" },
         use_default_keybindings = true,
         textobjects = { use_default_keybindings = true },
-        syntax_highlight = { enable = true },
-        shortsighted = false,
       })
     end,
     keys = {
       { "<leader>js", "<cmd>JupyniumStartAndAttachToServer<cr>", desc = "Start Jupynium" },
-      { "<leader>jS", "<cmd>JupyniumStartSync<cr>", desc = "Sync to notebook" },
+      { "<leader>jS", "<cmd>JupyniumStartSync<cr>", desc = "Sync to browser" },
       { "<leader>jx", "<cmd>JupyniumExecuteSelectedCells<cr>", desc = "Execute cell", mode = { "n", "x" } },
-      { "<leader>jX", "<cmd>JupyniumExecuteSelectedCells<cr><cmd>JupyniumScrollToCell<cr>", desc = "Execute & scroll" },
-      { "<leader>jc", "<cmd>JupyniumClearSelectedCellsOutputs<cr>", desc = "Clear outputs", mode = { "n", "x" } },
       { "<leader>jK", "<cmd>JupyniumKernelRestart<cr>", desc = "Restart kernel" },
       { "<leader>jI", "<cmd>JupyniumKernelInterrupt<cr>", desc = "Interrupt kernel" },
-      { "<leader>jw", "<cmd>JupyniumSaveIpynb<cr>", desc = "Save .ipynb" },
-      { "<leader>jd", "<cmd>JupyniumStopSync<cr>", desc = "Stop sync" },
     },
-    ft = { "python" },
   },
 
   -- which-key group
