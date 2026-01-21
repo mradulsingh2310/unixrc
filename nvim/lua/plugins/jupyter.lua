4e6208c58cca6183c9b9a21092e92f6e8746c876
Mradul Singh <msingh@dealmeridian.com>
2026-01-22 02:39:37 +0530

    Auto-sync [02:39]: nvim

--- nvim/lua/plugins/jupyter.lua
+++ nvim/lua/plugins/jupyter.lua
@@ -1,32 +1,28 @@
--- Jupyter Notebook Setup (from Reddit: molten + quarto + jupytext + image.nvim)
--- Open .ipynb -> auto-converts to markdown -> edit with LSP -> run with molten -> save back to .ipynb
+-- Jupyter Notebook Setup (minimal - without image.nvim for now)
 
 return {
-  -- 1. Jupytext: Auto-convert .ipynb <-> markdown on open/save
+  -- Jupytext: Convert .ipynb <-> markdown
   {
     "goerz/jupytext.nvim",
     version = "0.2",
+    lazy = false,
     opts = {
-      format = "md", -- Convert to markdown (works best with quarto)
-      update = true, -- Preserve outputs
-      autosync = true,
+      format = "py:percent", -- Python with # %% markers (faster than md)
+      update = true,
     },
   },
 
-  -- 2. Molten: Run code with Jupyter kernels, show output inline
+  -- Molten: Run code with Jupyter kernels
   {
     "benlubas/molten-nvim",
     version = "^1.0.0",
     build = ":UpdateRemotePlugins",
-    dependencies = { "3rd/image.nvim" },
     init = function()
-      vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_output_win_max_height = 20
       vim.g.molten_auto_open_output = false
       vim.g.molten_virt_text_output = true
-      vim.g.molten_virt_lines_off_by_1 = true
-      vim.g.molten_wrap_output = true
     end,
+    cmd = { "MoltenInit", "MoltenEvaluateLine" },
     keys = {
       { "<leader>ji", "<cmd>MoltenInit<cr>", desc = "Init kernel" },
       { "<leader>jl", "<cmd>MoltenEvaluateLine<cr>", desc = "Eval line" },
@@ -34,68 +30,7 @@ return {
       { "<leader>jr", "<cmd>MoltenReevaluateCell<cr>", desc = "Run cell" },
       { "<leader>jo", "<cmd>MoltenShowOutput<cr>", desc = "Show output" },
       { "<leader>jh", "<cmd>MoltenHideOutput<cr>", desc = "Hide output" },
-      { "<leader>jd", "<cmd>MoltenDelete<cr>", desc = "Delete output" },
-      { "<leader>jx", "<cmd>MoltenInterrupt<cr>", desc = "Interrupt kernel" },
     },
-  },
-
-  -- 3. Image.nvim: Render images (plots, charts) inline
-  {
-    "3rd/image.nvim",
-    opts = {
-      backend = "kitty", -- Use "kitty" or "ueberzug"
-      integrations = {
-        markdown = { enabled = true, clear_in_insert_mode = true },
-      },
-      max_width = 100,
-      max_height = 12,
-      window_overlap_clear_enabled = true,
-    },
-  },
-
-  -- 4. Quarto: LSP features (autocomplete, diagnostics) in code cells
-  {
-    "quarto-dev/quarto-nvim",
-    dependencies = {
-      "jmbuhr/otter.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    ft = { "quarto", "markdown" },
-    config = function()
-      require("quarto").setup({
-        lspFeatures = {
-          enabled = true,
-          languages = { "python" },
-          chunks = "all",
-          diagnostics = { enabled = true, triggers = { "BufWritePost" } },
-          completion = { enabled = true },
-        },
-        codeRunner = {
-          enabled = true,
-          default_method = "molten",
-        },
-      })
-
-      -- Auto-activate quarto for markdown files
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "markdown",
-        callback = function()
-          require("quarto").activate()
-        end,
-      })
-    end,
-    keys = {
-      { "<leader>jR", function() require("quarto.runner").run_cell() end, desc = "Run cell (quarto)" },
-      { "<leader>ja", function() require("quarto.runner").run_above() end, desc = "Run cells above" },
-      { "<leader>jA", function() require("quarto.runner").run_all() end, desc = "Run all cells" },
-    },
-  },
-
-  -- 5. Otter: LSP for embedded code (dependency of quarto)
-  {
-    "jmbuhr/otter.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    opts = {},
   },
 
   -- which-key group
