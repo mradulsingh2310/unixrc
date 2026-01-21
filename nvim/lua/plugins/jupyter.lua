f4845c99251d4926c3c7a5cc84ce89adbf07ccc9
Mradul Singh <msingh@dealmeridian.com>
2026-01-22 02:41:43 +0530

    Auto-sync [02:41]: nvim

--- nvim/lua/plugins/jupyter.lua
+++ nvim/lua/plugins/jupyter.lua
@@ -1,18 +1,18 @@
--- Jupyter Notebook Setup (minimal - without image.nvim for now)
+-- Jupyter Notebook Setup
+-- jupytext converts .ipynb to Python with # %% cell markers
 
 return {
-  -- Jupytext: Convert .ipynb <-> markdown
+  -- Jupytext: Convert .ipynb <-> Python
   {
     "goerz/jupytext.nvim",
     version = "0.2",
-    lazy = false,
     opts = {
-      format = "py:percent", -- Python with # %% markers (faster than md)
+      format = "py:percent",
       update = true,
     },
   },
 
-  -- Molten: Run code with Jupyter kernels
+  -- Molten: Run code with Jupyter kernels (lazy loaded)
   {
     "benlubas/molten-nvim",
     version = "^1.0.0",
@@ -22,24 +22,19 @@ return {
       vim.g.molten_auto_open_output = false
       vim.g.molten_virt_text_output = true
     end,
-    cmd = { "MoltenInit", "MoltenEvaluateLine" },
+    cmd = { "MoltenInit" },
     keys = {
       { "<leader>ji", "<cmd>MoltenInit<cr>", desc = "Init kernel" },
       { "<leader>jl", "<cmd>MoltenEvaluateLine<cr>", desc = "Eval line" },
       { "<leader>jv", ":<C-u>MoltenEvaluateVisual<cr>", desc = "Eval visual", mode = "v" },
       { "<leader>jr", "<cmd>MoltenReevaluateCell<cr>", desc = "Run cell" },
       { "<leader>jo", "<cmd>MoltenShowOutput<cr>", desc = "Show output" },
-      { "<leader>jh", "<cmd>MoltenHideOutput<cr>", desc = "Hide output" },
     },
   },
 
-  -- which-key group
+  -- which-key
   {
     "folke/which-key.nvim",
-    opts = {
-      spec = {
-        { "<leader>j", group = "jupyter" },
-      },
-    },
+    opts = { spec = { { "<leader>j", group = "jupyter" } } },
   },
 }
