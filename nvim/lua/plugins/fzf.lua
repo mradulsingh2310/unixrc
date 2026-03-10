98e0e23fd97dfee01d640f218f3e15243d7465fb
Mradul Singh <msingh@dealmeridian.com>
2026-03-10 23:23:39 +0530

    Auto-sync [23:23]: nvim

---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,11 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = {
+    winopts = {
+      winblend = 0, -- Override global winblend=10; keeps preview window fully opaque
+      preview = {
+        winblend = 0,
+      },
+    },
+  },
+}
